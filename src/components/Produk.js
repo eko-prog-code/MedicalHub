7fe8e50003d9c9b9c811d505f2339e25d0a8cee6
ruth-code-prog <ekosetiaji929@gmail.com>
2023-11-26 17:23:30 +0700

    oke

--- src/components/Produk.js
+++ src/components/Produk.js
@@ -1,29 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import TermModal from './TermModal'; // Import TermModal component
 import './Produk.css';
 
 const Produk = () => {
   const [produkList, setProdukList] = useState([]);
   const [currentTimestamp, setCurrentTimestamp] = useState('');
   const [searchKeyword, setSearchKeyword] = useState('');
+  const [selectedProduk, setSelectedProduk] = useState(null);
 
   useEffect(() => {
-    // Fetch data from Firebase Realtime Database
     const fetchData = async () => {
       try {
         const response = await axios.get('https://medicalhub-2023-default-rtdb.europe-west1.firebasedatabase.app/MedTradeHub.json');
-
-        // Response data is an object, convert it to an array
         const produkArray = Object.keys(response.data).map((key) => ({
           id: key,
           ...response.data[key],
         }));
 
-        setProdukList(produkArray.reverse()); // Reverse the array
-
-        // Set the current timestamp
-        updateCurrentTimestamp(); // Initial update
-        setInterval(updateCurrentTimestamp, 1000); // Update every second
+        setProdukList(produkArray.reverse());
+        updateCurrentTimestamp();
+        setInterval(updateCurrentTimestamp, 1000);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -52,13 +49,20 @@ const Produk = () => {
     setSearchKeyword(e.target.value);
   };
 
+  const openModal = (produk) => {
+    setSelectedProduk(produk);
+  };
+
+  const closeModal = () => {
+    setSelectedProduk(null);
+  };
+
   const filteredProdukList = produkList.filter((produk) =>
     produk.productName.toLowerCase().includes(searchKeyword.toLowerCase())
   );
 
   return (
     <div>
-      {/* Search form */}
       <div style={{ textAlign: 'center', marginBottom: '16px', width: '100%' }}>
         <input
           type="text"
@@ -68,18 +72,14 @@ const Produk = () => {
         />
       </div>
 
-      {/* Display current timestamp above the cards */}
       <div style={{ textAlign: 'center', marginBottom: '16px', width: '100%' }}>
         <p>Tanggal dan Waktu Terkini: {currentTimestamp}</p>
       </div>
 
-      {/* Product cards */}
       <div className="produk-container">
         {filteredProdukList.map((produk) => (
-          <div key={produk.id} className="produk-card">
-            {/* Display the timestamp of the product */}
+          <div key={produk.id} className="produk-card" onClick={() => openModal(produk)}>
             <p style={{ fontSize: '12px', marginBottom: '8px' }}>Tanggal Terbit Penjualan: {produk.timestamp}</p>
-
             <h3>{produk.productName}</h3>
             <p>{produk.description}</p>
             <p>Quantity: {produk.quantity}</p>
@@ -97,6 +97,9 @@ const Produk = () => {
           </div>
         ))}
       </div>
+
+      {/* Modal */}
+      <TermModal isOpen={selectedProduk !== null} closeModal={closeModal} />
     </div>
   );
 };
---
+++ src/components/TermModal.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import './Term.css';
+
+Modal.setAppElement('#root'); // Untuk menghindari a11y error
+
+const TermModal = ({ isOpen, closeModal }) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={closeModal}
+      className="modal"
+      overlayClassName="overlay"
+    >
+      <div className="modal-content">
+        <span className="close-button" onClick={closeModal}>
+          &times;
+        </span>
+        <h2>Syarat dan Ketentuan Pengguna Medical Hub:</h2>
+        <ol>
+          <li>Penjual harus memposting produk dengan biodata yang akurat dan valid. Informasi yang tidak benar tidak diizinkan.</li>
+          <li>Medical Hub dan PT.InnoView Indo Tech tidak bertanggung jawab atas kecurangan antara penjual dan pembeli. Platform ini hanya sebagai media informasi dan tidak terlibat dalam konflik perdata jual beli.</li>
+          <li>Transaksi melibatkan Medical Hub harus mengikuti prosedur resmi dari PT.InnoView Indo Tech. Penjual dan pembeli diharapkan mengisi formulir transaksi secara lengkap. Untuk informasi lebih lanjut, hubungi: 0895600394345.</li>
+        </ol>
+        <p>Harap memilih opsi 2 atau 3 dalam bertransaksi sesuai preferensi Anda. Penting untuk mengisi biodata dengan benar sesuai dengan poin 1.</p>
+        <p>Terima kasih atas perhatiannya.</p>
+        <p>Syarat dan Ketentuan ini berlaku sesuai Undang-Undang di negara Indonesia.</p>
+      </div>
+    </Modal>
+  );
+}
+
+export default TermModal;
